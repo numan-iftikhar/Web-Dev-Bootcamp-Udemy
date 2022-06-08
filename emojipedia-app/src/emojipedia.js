8002b473863e48c383f4dd97bc1212a0b0a86a2a
numan-iftikhar <numaniftikhar300@gmail.com>
2022-06-09 00:36:30 +0500

    complete emojipedia app

--- emojipedia-app/src/emojipedia.js
+++ emojipedia-app/src/emojipedia.js
@@ -19,5 +19,27 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  }
+  },
+  {
+    id: 4,
+    emoji: "❤️",
+    name: "Red Heart",
+    meaning: "A classic red love heart emoji, used for expressions of love and romance. This is the most popular heart emoji. A similar emoji exists for the heart suit in a deck of playing cards."
+  },
+  {
+    id: 5,
+    emoji: "✨",
+    name: "Sparkles",
+    meaning:
+      "The glittering flashes of sparkles. Generally depicted as a cluster of three, yellow four-point stars, with one large sparkle and two small ones to its left or right."
+  },
+  {
+    id: 6,
+    emoji: "♨️",
+    name: "Hot Springs",
+    meaning:
+      "Representation of hot springs with steam rising from a circular tub. This is the common symbol used to represent an onsen on maps in Japan."
+  },
 ];
+
+export default emojipedia;
