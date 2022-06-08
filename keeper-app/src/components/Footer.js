0bf673198de01efeb639c8834a1fc6ed2d78d376
numan-iftikhar <numaniftikhar300@gmail.com>
2022-06-08 22:28:05 +0500

    add keeper app

---
+++ keeper-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+let year = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>copyright &copy; {year}</p>
+    </footer>
+  )
+}
+
+export default Footer
