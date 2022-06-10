92fa6e389f5f5b875df1b77ed2978d5fc45a6d93
numan-iftikhar <numaniftikhar300@gmail.com>
2022-06-10 20:59:43 +0500

    remove extra files and add required files in keeper-app

--- keeper-app/src/App.js
+++ keeper-app/src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 function App() {
   const [notes, setNotes] = useState([]);
 
