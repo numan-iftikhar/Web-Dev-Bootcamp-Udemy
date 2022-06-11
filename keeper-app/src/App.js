5f6816fe8d1278038de9fcd3c922b3d21024478b
numan-iftikhar <numaniftikhar300@gmail.com>
2022-06-11 09:00:03 +0500

    minor tweaking

--- keeper-app/src/App.js
+++ keeper-app/src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
+
+
 function App() {
   const [notes, setNotes] = useState([]);
 
